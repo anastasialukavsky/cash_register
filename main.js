79f72aeded8ee68ed90b1e5096e01090e87bcba2
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-12 20:14:59 -0400

    solve lvl3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//LVL 1
+//*LVL 1
 function removeItem(name, drawer) {
   for (let item of drawer) {
     if (item.name === name) item.quantity--;
@@ -22,9 +22,9 @@ function addItem(name, drawer) {
 
 const drawerToAdd = [{ name: 'nickel', value: 5, quantity: 5 }];
 const updatedDrawer = addItem('nickel', drawerToAdd); // Adds 1 nickel
-console.log(updatedDrawer); // [{ name: 'nickel', value: 5, quantity: 6 }];
+console.log({updatedDrawer}); // [{ name: 'nickel', value: 5, quantity: 6 }];
 
-//LVL2
+//*LVL2
 
 const drawerLvl2 = [
   { name: 'penny', value: 1, quantity: 72 },
@@ -59,4 +59,28 @@ const notes = countNotes(drawerLvl2); // Returns 107
 console.log({ coins });
 console.log({ notes });
 
+//*LVL3
+const drawerSum = [
+  { name: 'penny', value: 1, quantity: 72 },
+  { name: 'nickel', value: 5, quantity: 41 },
+  { name: 'dime', value: 10, quantity: 31 },
+  { name: 'quarter', value: 25, quantity: 17 },
+  { name: 'one', value: 100, quantity: 90 },
+  { name: 'five', value: 500, quantity: 11 },
+  { name: 'ten', value: 1_000, quantity: 2 },
+  { name: 'twenty', value: 2_000, quantity: 3 },
+  { name: 'hundred', value: 10_000, quantity: 1 },
+];
+function sumDrawer(drawer) {
+  let sum = 0;
+  for (let item of drawer) {
+    let totalValue = (item.quantity * item.value) / 100;
+    sum += totalValue;
+  }
+  let result = sum.toFixed(2);
+  return `\$${result}`;
+}
 
+
+const total = sumDrawer(drawerSum); // Returns '$335.12'
+console.log({total})
